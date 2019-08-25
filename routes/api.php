52f837dda787713cbb665b30a02015e00f987b39
matrizlab <info@matrizlab.com>
2019-08-25 13:04:08 +0200

    some endpoint using resource routes

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,45 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+/**
+ * Customers
+ */
+
+Route::resource('customers', 'Customer\CustomerController', ['only' => ['index', 'show']]);
+
+/**
+ * Providers
+ */
+
+Route::resource('providers', 'Provider\ProviderController', ['only' => ['index', 'show']]);
+
+/**
+ * Invoices
+ */
+
+Route::resource('invoices', 'Invoice\InvoiceController', ['only' => ['index', 'show']]);
+
+/**
+ * Products
+ */
+
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+/**
+ * Categories
+ */
+
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+/**
+ * Payments
+ */
+
+Route::resource('payments', 'Payment\PaymentController', ['only' => ['index', 'show']]);
+
+/**
+ * Users
+ */
+
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
